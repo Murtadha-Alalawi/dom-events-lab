92f454456715ed439b49e5ef40768380432bcda0
Murtadha-Alalawi <murtadha.alawi04@gmail.com>
2024-11-26 21:06:26 +0300

    git commit

--- app.js
+++ app.js
@@ -1,46 +1,102 @@
 
-const buttons = document.querySelectorAll('.button');
+const numberButtons = document.querySelectorAll('.number');
 const calculator = document.querySelector('#calculator');
 const display = document.querySelector(".display");
+const operationButtons = document.querySelectorAll(".operator")
+const equalButton = document.querySelector(".equals")
+const deleteButton = document.querySelector(".delete")
+console.log(equalButton)
+let firstNum
+let operation = null
+let secondNum
+let total
 
 
-const firstNum = 0
-const operation = null 
-const secondNum = 0
 
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-     const value = event.target.innerText
-     console.log(value)
-    if(!isNaN(value)){
-    if(display.innerText === '0'){
-      display.innerText = value
-     }
 
-     else{
-      display.innerText += value
-     }}
-    });
-  });
+    numberButtons.forEach((button)=>{
+      button.addEventListener('click', (event)=>{
+        if(!firstNum){
+          firstNum = event.target.innerText
+        }
+        
 
+        if(display.innerText === ''){
+          display.innerText = firstNum
+        } else if (operation === null) {
+          display.innerText += event.target.innerText
+          firstNum += event.target.innerText
+          console.log(firstNum)
+        }
+    
+        if(firstNum && operation !== null){
+          if(!secondNum){ 
+            secondNum = event.target.innerText
+            display.innerText = secondNum
+          } else {
+            secondNum += event.target.innerText
+          display.innerText += event.target.innerText
+          }
 
-  calculator.addEventListener('click', (event) => {
-    ;
-  
-    if (event.target.classList.contains('number')) {
+          
+         }
+        
       
+      })
+    })
+    operationButtons.forEach((button)=>{
+      button.addEventListener("click",(event)=>{
+        if(display.innerText === firstNum){
+          operation = event.target.innerText
+          console.log(operation)
+          display.innerText = ''
+        }
+      
+       
+        // continue second number IF first is true and operation is ture
+
+      })
+    })
+
+   
+   equalButton.addEventListener("click", (event)=>{
+    if(firstNum !== "" && operation !== "" && secondNum !== ""){
+
+      if(operation === "+"){
+
+       total =(Number(firstNum) + Number(secondNum))
+       display.textContent=total
+      }
+      else if(operation === "-"){
+       total = (Number(firstNum) - Number(secondNum))
+       display.textContent=total
+      }
+
+      else if(operation === "*"){
+        total = (Number(firstNum) * Number(secondNum))
+       display.textContent=total
+      }
+
+      else if(operation === "/"){
+        total = (Number(firstNum) / Number(secondNum))
+        display.textContent=total
+      }
+
     }
-  
-    if (event.target.innerText === '*') {
-    }
-  });
-  
+   })
+
+
+   deleteButton.addEventListener("click", (event) => {
+     if(display.innerText !== ""){
+     display.innerText = null
+     }
+   })
+   
+
   
 
 
-
-const total = 0
 
 
 
